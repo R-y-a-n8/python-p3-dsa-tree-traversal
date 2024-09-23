14401ed4445169b64293ff8959fef255df7ecb0d
R-y-a-n8 <ryan.kinuthia@student.moringaschool.com>
2024-09-23 23:40:41 +0300

    tree

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,15 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, node_id):
+        return self._depth_first_search(self.root, node_id)
+
+    def _depth_first_search(self, node, node_id):
+        if node['id'] == node_id:
+            return node
+        for child in node['children']:
+            result = self._depth_first_search(child, node_id)
+            if result:
+                return result
+        return None
